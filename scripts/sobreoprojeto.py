35a2674dd92bb4242db2ba7b683b44e7be8606a6
Gustavo Oliveira <gutti.bp@gmail.com>
2025-02-06 12:46:32 -0300

    Update sobreoprojeto.py

--- scripts/sobreoprojeto.py
+++ scripts/sobreoprojeto.py
@@ -27,8 +27,7 @@ def show():
         <div style="text-align: justify;">
             <p>
                 O Datathon propõe a criação de uma proposta preditiva. Os participantes criarão um modelo preditivo para prever o comportamento dos estudantes com base em variáveis cruciais. A ideia é utilizar conhecimentos aprendidos no curso, como técnicas de machine learning, deep learning ou processamento de linguagem natural, para propor soluções de algoritmos supervisionados ou não supervisionados.
-                
-        
+            </p>
         </div>
         ''',
         unsafe_allow_html=True
@@ -38,20 +37,21 @@ def show():
     st.markdown(
         '''
         <div style="text-align: justify;">
-   <p>
-    Os dados foram obtidos das informações educacionais e socioeconômicas dos estudantes da Passos Mágicos, incluindo:
-</p>
-<p>
-    Duas bases de dados com as características de desenvolvimento educacional e questões socioeconômicas dos estudantes e um dicionário de dados com o mapeamento de todas as variáveis.
-</p>
-
-<ul>
-    <li>
-        <b><a style='text-decoration:none;color:blue' href='https://drive.google.com/drive/folders/1Z1j6uzzCOgjB2a6i3Ym1pmJRsasfm7cD'>Bases de Dados</a></b>.
-    </li>
-</ul>
-
-
+            <p>
+                Os dados foram obtidos das informações educacionais e socioeconômicas dos estudantes da Passos Mágicos, incluindo:
+            </p>
+            <p>
+                Duas bases de dados com as características de desenvolvimento educacional e questões socioeconômicas dos estudantes e um dicionário de dados com o mapeamento de todas as variáveis.
+            </p>
+            <ul>
+                <li>
+                    <b><a style='text-decoration:none;color:blue' href='https://drive.google.com/drive/folders/1Z1j6uzzCOgjB2a6i3Ym1pmJRsasfm7cD'>Bases de Dados</a></b>.
+                </li>
+            </ul>
+        </div>
         ''',
         unsafe_allow_html=True
     )
+
+# Chamando a função show() para exibir o conteúdo
+show()
